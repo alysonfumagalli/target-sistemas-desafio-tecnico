a79da3b3cd51b89072024b679e8431ddca63bafb
alyson <alysoncesarfumagalli@gmail.com>
2024-10-23 15:05:44 -0300

    Melhorias realizadas no Desafio 1

--- desafio1/src/desafio1/Fibonacci.java
+++ desafio1/src/desafio1/Fibonacci.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 
 public class Fibonacci {
 	public static void main(String[] args) {
+		try {
 			Scanner input = new Scanner(System.in);
-			System.out.print("> Informe um número a ser pesquisado na Sequência de Fibonacci: ");
+			System.out.print("> Informe um número (inteiro) a ser pesquisado na Sequência de Fibonacci: ");
 			Integer number = Integer.parseInt(input.nextLine());
 			input.close();
 			boolean result = isThisNumberInFibonacci(number);
-			if (result) {
-				System.out.println(String.format("> O número %d está presente na Sequência de Fibonacci!", number));
-			} else {
-				System.out.println(String.format("> O número %d não está presente na Sequência de Fibonacci...", number));
-			}
+			showResult(number, result);
+		} catch (NumberFormatException e) {
+			System.err.println("> Por favor, informe um valor válido...");
+			main(args);
+		}
+	}
+
+	private static void showResult(Integer number, boolean result) {
+		if (result) {
+			System.out.println(String.format("> O número %d está presente na Sequência de Fibonacci!", number));
+		} else {
+			System.out.println(String.format("> O número %d não está presente na Sequência de Fibonacci...", number));
+		}
 	}
 	
-	public static boolean isThisNumberInFibonacci(int givenNumber) {
+	private static boolean isThisNumberInFibonacci(int givenNumber) {
 		int currentNumber = 1;
 		int previousNumber = 0;
 		while (currentNumber <= givenNumber) {
